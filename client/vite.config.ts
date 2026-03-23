1d2753e3441c868fb190a4711ddcbae50ee461ae
usersina <benna.anis2@gmail.com>
2026-03-23 20:31:59 +0100

    feat: add Taskfile for ngrok setup and configure server settings in vite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,16 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['nicely-tops-terrapin.ngrok-free.app'],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
